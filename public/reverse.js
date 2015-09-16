75cb23d391ae8d8dcf266ad9d42a6e17d13e0ca6
natashalynne <hakun.natasha@gmail.com>
2015-09-16 19:15:33 -0400

    Complete hard mode.

--- public/reverse.js
+++ public/reverse.js
@@ -4,7 +4,12 @@
   Note: No using string.reverse() or array.reverse(), that's cheating :)
 */
 var reverseWord = function(word) {
-
+  var newWord = word.split("");
+  var reverseWord = "";
+  for (i = word.length - 1; i >= 0; i--) {
+    reverseWord += newWord[i];
+  }
+  return reverseWord;
 }
 
 
@@ -16,5 +21,12 @@ var reverseWord = function(word) {
   - Return a string that contains each reversed word, in order
 */
 var reverseSentence = function(sentence) {
-
+  var sentenceSplit = [];
+  var words = "";
+  sentenceSplit = sentence.split(" ");
+  for (j = 0; j < sentenceSplit.length; j++) {
+    words += reverseWord(sentenceSplit[j]) + " ";
+  }
+  return words.trim();
 }
+ reverseSentence("My name is Natasha");
